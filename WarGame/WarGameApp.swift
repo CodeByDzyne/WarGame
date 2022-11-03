bc96563b4979dadddee6ff63a3a4e40a01fb955c
CodeByDzyne <wdenley@dzynesolutions.com>
2022-11-03 18:23:48 +1100

    Initial Commit

---
+++ WarGame/WarGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  WarGameApp.swift
+//  WarGame
+//
+//  Created by Wealthy Waz on 3/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct WarGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
